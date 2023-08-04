9742f177d6885d6eabe5cf2904338e6930019245
jrukeijakare <juniorjoe567@gmail.com>
2023-08-04 17:41:22 +0300

    updates to contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,16 +37,16 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15958.93892067135!2d32.62879434534002!3d0.3708851513804115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177db9effd93d32b%3A0x235f5285392e9a96!2sNaalya%2C%20Kampala!5e0!3m2!1sen!2sug!4v1691159513716!5m2!1sen!2sug"
           />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md" style={{width:450}}>
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                Naalya <br />
+                Wakiso, Uganda
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -54,12 +54,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                rukejoseph@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">+256 770 963 149</p>
             </div>
           </div>
         </div>
